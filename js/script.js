1a9477c943cb290dd7450c841946e195447305a3
Byan Aditya <aditya.byan00@gmail.com>
2025-07-21 15:57:47 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -164,4 +164,15 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-                                     
+// === MOUSE MOVE===
+document.addEventListener("DOMContentLoaded", () => {
+  const cursor = document.createElement("div");
+  cursor.classList.add("custom-cursor");
+  document.body.appendChild(cursor);
+
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.top = `${e.clientY}px`;
+    cursor.style.left = `${e.clientX}px`;
+  });
+});
+
